e946e2b31c1496e71000a4b6750b68c2bf5cba3a
Jean-Olivier Irisson <irisson@normalesup.org>
2024-10-12 23:19:22 +0200

    Add range to getting the exported files to allow resuming

--- R/get_job_file.R
+++ R/get_job_file.R
@@ -5,9 +5,10 @@
 #' **Return the file produced by given job.** 🔒 The job must be accessible to current user.
 #'
 #' @param job_id integer; Internal, the unique numeric id of this job.
+#' @param Range string; NULL
 #'
 #' @export
-get_job_file <- function(job_id) {
+get_job_file <- function(job_id, Range = NULL) {
   handle_api_response(
     httr2::request(base_url = paste0(api_url(), "/jobs/", job_id, "/file")) %>%
       httr2::req_method("GET") %>%
